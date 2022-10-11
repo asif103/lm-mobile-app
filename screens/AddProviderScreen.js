e269ef8ebec11e69474b259567f5546a49910274
Mohammed Asif <asif.cse473@gmail.com>
2022-10-11 09:34:20 +0600

    changes

---
+++ screens/AddProviderScreen.js
@@ -0,0 +1,85 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableOpacity, ScrollView
+} from "react-native";
+import HeaderComponent from "../components/HeaderComponent";
+import userImage from "../assets/user.png";
+import { Ionicons } from '@expo/vector-icons';
+import ConnectedProviderScreen from "../components/ConnectedProviderScreen";
+const AddProviderScreen = ({ navigation }) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      
+        <TouchableOpacity style={styles.addButton}>
+          <Ionicons name="ios-add-circle" size={50} color="#77ACA2" />
+        </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default AddProviderScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+    paddingTop: 50,
+    position: "relative"
+  },
+  containerInner:{
+    padding:30,
+    marginBottom:20
+  },
+  emptySpace:{
+    height:50
+  },
+  addButton:{
+    position:"absolute",
+    bottom:50,
+    right:30
+  },
+  subTitle: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  title: {
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+  paragraph: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    fontSize: 16,
+    textAlign: "center",
+  },
+  mainContentContainer: {
+    paddingTop: 20,
+    width: "90%",
+  },
+  userCard: {
+    width: "90%",
+    padding: 20,
+    marginTop: 20,
+    backgroundColor: "#F5F5F5",
+    borderRadius: 6,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  userCardInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  userCardImage: {
+    height: 36,
+    width: 36,
+    borderRadius: 36 / 2,
+    marginRight: 10,
+  },
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,99 +1,115 @@
 import React from "react";
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  SafeAreaView,
-  TouchableOpacity, ScrollView,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    SafeAreaView,
+    TouchableOpacity, ScrollView,
 } from "react-native";
 import HeaderComponent from "../components/HeaderComponent";
 import userImage from "../assets/user.png";
-import { AntDesign } from "@expo/vector-icons";
+import {AntDesign, Ionicons} from "@expo/vector-icons";
 import ConnectedClientScreen from "../components/ConnectedClientScreen";
-const HomeScreen = ({ navigation }) => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <HeaderComponent
-        navigation={navigation}
-        greet={"Good Morning"}
-        name={"Sam Samson"}
-        imageFile={userImage}
-      />
-      <ScrollView style={styles.containerInner}>
-      <View style={styles.mainContentContainer}>
-        <Text style={styles.title}>Connected Profiles</Text>
-      </View>
-      <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
-      <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
-      <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
-      <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
-      <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
-      <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
-      <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
-      <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
-      <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
-      <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
-      <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
-      <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
-      <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
-      </ScrollView>
-    </SafeAreaView>
-  );
+
+const HomeScreen = ({navigation}) => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <HeaderComponent
+                navigation={navigation}
+                greet={"Good Morning"}
+                name={"Sam Samson"}
+                imageFile={userImage}
+            />
+            <ScrollView style={styles.containerInner}>
+                <View style={styles.mainContentContainer}>
+                    <Text style={styles.title}>Connected Profiles</Text>
+                </View>
+                <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
+                <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
+                <ConnectedClientScreen navigation={navigation} userImage={userImage} title={'sam samson'}/>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Add another client profile</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.emptySpace}/>
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    backgroundColor: "#ffffff",
-    paddingTop: 50,
-  },
-  containerInner:{
-    padding:30,
-    marginBottom:20
-  },
-  emptySpace:{
-    height:50
-  },
-  subTitle: {
-    fontSize: 22,
-    fontWeight: "bold",
-  },
-  title: {
-    fontSize: 17,
-    fontWeight: "bold",
-  },
-  paragraph: {
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    fontSize: 16,
-    textAlign: "center",
-  },
-  mainContentContainer: {
-    paddingVertical: 20,
-    width: "90%",
-  },
-  userCard: {
-    width: "90%",
-    padding: 20,
-    marginTop: 20,
-    backgroundColor: "#F5F5F5",
-    borderRadius: 6,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  userCardInfo: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  userCardImage: {
-    height: 36,
-    width: 36,
-    borderRadius: 36 / 2,
-    marginRight: 10,
-  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "#ffffff",
+        paddingTop: 50,
+    },
+    containerInner: {
+        padding: 30,
+        marginBottom: 20
+    },
+    emptySpace: {
+        height: 50
+    },
+    addButton:{
+    },
+    subTitle: {
+        fontSize: 22,
+        fontWeight: "bold",
+    },
+    title: {
+        fontSize: 17,
+        fontWeight: "bold",
+    },
+    paragraph: {
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        fontSize: 16,
+        textAlign: "center",
+    },
+    mainContentContainer: {
+        paddingVertical: 20,
+        width: "90%",
+    },
+    userCard: {
+        width: "90%",
+        padding: 20,
+        marginTop: 20,
+        backgroundColor: "#F5F5F5",
+        borderRadius: 6,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    userCardInfo: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    userCardImage: {
+        height: 36,
+        width: 36,
+        borderRadius: 36 / 2,
+        marginRight: 10,
+    },
+    button: {
+        backgroundColor: "#77ACA2",
+        paddingVertical: 15,
+        width: "90%",
+        borderRadius: 9,
+    },
+    buttonText: {
+        color: "#ffffff",
+        fontWeight: "bold",
+        fontSize: 16,
+        textAlign: "center"
+    },
+    buttonContainer:{
+        width:'90%',
+        alignItems:"center",
+        paddingVertical:20
+    }
 });
