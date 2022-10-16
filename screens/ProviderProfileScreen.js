8a1425f8fe26db15cb88726e795b1523827ccf3e
Mohammed Asif <asif.cse473@gmail.com>
2022-10-16 09:39:06 +0600

    provider header done

--- screens/ProviderProfileScreen.js
+++ screens/ProviderProfileScreen.js
@@ -8,7 +8,10 @@ import {
     TouchableOpacity, ScrollView, TextInput
 } from "react-native";
 import providerImage from '../assets/provider.png'
-
+import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 const ProviderProfileScreen = ({navigation}) => {
     React.useLayoutEffect(() => {
         navigation.setOptions({
@@ -24,6 +27,31 @@ const ProviderProfileScreen = ({navigation}) => {
                     <View style={styles.headerImageContainer}>
                         <Image source={providerImage} style={styles.providerImageStyle}/>
                     </View>
+                    <View style={styles.headerInformation}>
+                        <View>
+                            <Text style={styles.providerName}>Speech Pathologist</Text>
+                            <Text style={styles.providerSubtitle}>The Example Health Care
+                                Centre</Text>
+                        </View>
+                        <View style={styles.headerIcons}>
+                            <TouchableOpacity style={styles.iconContainer}>
+                                <MaterialIcons name="phone-in-talk" size={24} color="#ffffff" />
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.iconContainer}>
+                                <MaterialCommunityIcons name="email-outline" size={24} color="#ffffff" />
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.iconContainer}>
+                                <Entypo name="location" size={24} color="#ffffff" />
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.iconContainer}>
+                                <Feather name="globe" size={24} color="#ffffff" />
+                            </TouchableOpacity>
+
+                        </View>
+                    </View>
+                </View>
+                <View>
+                    
                 </View>
             </ScrollView>
         </SafeAreaView>
@@ -42,10 +70,43 @@ const styles = StyleSheet.create({
     mainContainer: {
         width: "100%"
     },
-    headerContainer: {},
-    headerImageContainer: {},
+    headerContainer: {
+        flexDirection:"row",
+        padding:20
+    },
+    headerImageContainer: {
+        marginRight:20
+    },
     providerImageStyle: {
         width: 105,
         height: 113
+    },
+    headerInformation:{
+
+    },
+    headerIcons:{
+        marginTop:10,
+        flexDirection:"row"
+    },
+    providerName:{
+        fontSize:17,
+        fontWeight:"bold"
+    },
+    providerSubtitle:{
+        fontSize:14,
+        color: '#505050'
+    },
+    iconContainer:{
+        height:40,
+        width:40,
+        marginRight:10,
+        borderRadius:20,
+        backgroundColor:'#77ACA2',
+        display:"flex",
+        alignItems:'center',
+        justifyContent:"center",
+
+
     }
+
 });
